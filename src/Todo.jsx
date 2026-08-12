b06684df42ae45a637a92a4db2b0255febd70025
limonreza <limon4reza000@gmail.com>
2026-08-12 17:13:25 +0600

    conditional rendering

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,5 +1,12 @@
-export default function ToDo(props){
-    return(
-        <li> TAsk: {props.task}</li>
-    )
+// export default function ToDo({task, isActive}){
+//     return(
+//         <li>Task: {task}</li>
+//     )
+// }
+
+export default function Foods ({food, isHungry, time=0}){
+    if(isHungry==true){
+        return<li>Done: {food} Duration: {time} minutes</li>
+    }
+    return <li>Not done: {food}</li>
 }
