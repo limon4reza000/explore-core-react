6a6ed6bcde508188ce8348fde95150faab3a4166
limonreza <limon4reza000@gmail.com>
2026-08-12 17:25:38 +0600

    conditional rendering with ternary operator

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -4,9 +4,16 @@
 //     )
 // }
 
-export default function Foods ({food, isHungry, time=0}){
-    if(isHungry==true){
-        return<li>Done: {food} Duration: {time} minutes</li>
-    }
-    return <li>Not done: {food}</li>
+// export default function Foods ({food, isHungry, time=0}){
+//     if(isHungry==true){
+//         return<li>Done: {food} Duration: {time} minutes</li>
+//     }
+//     return <li>Not done: {food}</li>
+// }
+
+
+export default function ToDo({ task, isDone, time = 0}) {
+    return isDone ?
+        <li>Done : {task} time : {time} </li>
+        : <li>Not done : {task} time : {time} </li>
 }
